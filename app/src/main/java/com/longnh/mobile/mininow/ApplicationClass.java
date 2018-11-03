9df086f0ca4d4876e505bd3738a64af1535cb526
Yuu <lyokha113@gmail.com>
2018-11-03 13:22:23 +0700

    Init App

---
+++ app/src/main/java/com/longnh/mobile/mininow/ApplicationClass.java
@@ -0,0 +1,19 @@
+package com.longnh.mobile.mininow;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class ApplicationClass extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
